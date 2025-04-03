27f60bcc0cce2109d2c0e2f7b1c85e3494a2c463
isaac-kwarteng <kwartzadey1@gmail.com>
2025-04-03 04:20:44 +0000

    Add unit tests for ShoppingCart methods

--- New assignnments/test_call.py
+++ New assignnments/test_call.py
@@ -4,6 +4,3 @@ def test_call():
     cart = ShoppingCart({"apple": 10, "banana": 5})
     assert cart() == 15
 
-if __name__ == "__main__":
-    test_call()
-    print("test_call passed!")
--- New assignnments/test_len.py
+++ New assignnments/test_len.py
@@ -3,7 +3,3 @@ from shopping_cart import ShoppingCart
 def test_len():
     cart = ShoppingCart({"apple": 10, "banana": 5})
     assert len(cart) == 2
-
-if __name__ == "__main__":
-    test_len()
-    print("test_len passed!")
